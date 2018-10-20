ea64e6ba6c16ff0fab723f24a607d50b382477a7
Varun <vskred126@gmail.com>
2018-10-20 06:58:41 -0400

    code for not-working bot as of Saturday at 1 am...commiting late

--- rootjs/test-json.js
+++ rootjs/test-json.js
@@ -3,7 +3,22 @@ var controller = botkit.botframeworkbot({debug: true});
 var bot = controller.spawn({
     appId: process.env.app_id,
 });
+controller.on();
+
 
 controller.hears('hello', 'direct_message', function (bot, message) {
     bot.reply(message, 'Hello yourself');
+});
+
+controller.middleware.receive.use(function(bot, message, next) {
+
+    // log it
+    console.log('RECEIVED: ', message);
+
+    // modify the message
+    message.logged = true;
+
+    // continue processing the message
+    next();
+
 });
